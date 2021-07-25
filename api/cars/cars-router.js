9335f1ac9cc3434bf8fd290b6611ce881d17adf5
PVigar88 <Pvigar@gmail.com>
2021-07-25 18:20:55 -0400

    added cars router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,49 @@
 // DO YOUR MAGIC
+
+const router = require("express)").Router();
+const Cars = require("./cars-model");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
+
+router.get("/", (req, res, next) => {
+  Cars.getAll()
+    .then((cars) => {
+      res.status(200).json(cars);
+    })
+    .catch((err) => {
+      console.log(err);
+      next({ errorMessage: "There was a server error (cars-gt)" });
+    });
+});
+
+router.get("/:id", checkCarId, (req, res, next) => {
+  Cars.getById(req.params.id)
+    .then((car) => {
+      res.status(200).json(car);
+    })
+    .catch((err) => {
+      console.log(err);
+      next({ errorMessage: "There was a server error (cars-gtid)" });
+    });
+});
+
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  (req, res, next) => {
+    Cars.create(req.body)
+      .then((car) => {
+        res.status(201).json(car);
+      })
+      .catch((err) => {
+        console.log(err);
+        next({ errorMessage: "There was a server error (cars-pst)" });
+      });
+  }
+);
